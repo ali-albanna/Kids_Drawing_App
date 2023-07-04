06010383c0cbd763b2f5a27482e0e0e91df02249
Ali <alialsayyid@gmail.com>
2023-07-04 19:45:42 +0300

    Commit

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.kidsdrawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +17,32 @@ class MainActivity : AppCompatActivity() {
 
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20f)
+
+        val ib_brush: ImageButton = findViewById(R.id.ib_brush)
+        ib_brush.setOnClickListener { showBrushSizeChooserDialog() }
     }
+
+    private fun showBrushSizeChooserDialog() {
+        var brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+
+        val smallBtn: ImageButton = brushDialog.findViewById(R.id.ib_small_brush)
+        val mediumBtn: ImageButton = brushDialog.findViewById(R.id.ib_medium_brush)
+        val largeBtn: ImageButton = brushDialog.findViewById(R.id.ib_large_brush)
+        smallBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(10f)
+            brushDialog.dismiss()
+        }
+        mediumBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(20f)
+            brushDialog.dismiss()
+        }
+        largeBtn.setOnClickListener {
+            drawingView?.setSizeForBrush(30f)
+            brushDialog.dismiss()
+        }
+        brushDialog.show()
+    }
+
 }
